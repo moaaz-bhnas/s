a5440b2274696493fd4f626ed3b89234c1da1500
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-03-15 18:50:21 +0200

    Updates

---
+++ precache-manifest.b6bac146435e828b316e63c31c382d54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d8436d1c5b83a5ed98962866ffbb66",
+    "url": "/s/index.html"
+  },
+  {
+    "revision": "ac0e9c5a8546631f0bcc",
+    "url": "/s/static/js/2.4c839f17.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/s/static/js/2.4c839f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46304c2c56a00888824a",
+    "url": "/s/static/js/main.3760fed2.chunk.js"
+  },
+  {
+    "revision": "7f66d69b065dfaa2fea7",
+    "url": "/s/static/js/runtime-main.2e8f3a99.js"
+  },
+  {
+    "revision": "c8da9bc74d689e70836adc32b4925452",
+    "url": "/s/static/media/abds.c8da9bc7.jpg"
+  }
+]);
